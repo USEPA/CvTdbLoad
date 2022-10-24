6e04e482e6d3ade8091537ba64875318924b07cd
Taylor Wall <wall.jonathan@epa.gov>
2022-10-24 14:10:18 -0400

    Update of tk_params SQL schema changes

--- scripts/r_load_CvT/update_tk_params.R
+++ scripts/r_load_CvT/update_tk_params.R
@@ -1,5 +1,5 @@
 library(readr); library(dplyr)
-cvt_in = readr::read_delim("input\\tk params\\SupTable-CvTData.txt")
-tk_params = readr::read_delim("input\\tk params\\SupTable-TKFits.txt") %>% 
+cvt_in = readr::read_delim("input\\tk params\\SupTable-CvTData.txt", show_col_types = FALSE)
+tk_params = readr::read_delim("input\\tk params\\SupTable-TKFits.txt", show_col_types = FALSE) %>% 
   select(DTXSID, Compound, CAS, Species, Reference, Model, halflife) %>%
   distinct()
