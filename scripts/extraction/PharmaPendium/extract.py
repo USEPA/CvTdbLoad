4d935447dd2fc5cd2be4b5451128746fbf5bb584
Taylor Wall <wall.jonathan@epa.gov>
2023-10-26 14:22:17 -0400

    Updates to make script more dynamic.

--- scripts/extraction/PharmaPendium/extract.py
+++ scripts/extraction/PharmaPendium/extract.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import pandas as pd
+import numpy as np
 import os
 import random
 import re
@@ -7,25 +8,18 @@ import warnings
 
 load_dotenv(".env")
 
-
 def extract_documents_sheet(df):
     documents = df.groupby(["Document"], dropna=False).head(1)
     num_documents = len(documents)
 
-    # Extract id column
+    # Set id column
     ids = [i for i in range(1, num_documents + 1)]
 
-    # Extract document_type column
+    # Set document_type column
     document_types = [1] * num_documents
-
-    # Extract pmid column
-    pmids = [""] * num_documents
 
     # Extract other_study_identifier columns
     other_study_identifiers = documents["Study Number"].tolist()
-
-    # Extract extracted column
-    extracteds = [""] * num_documents
 
     # Extract doi column
     source_links = documents["Source Link"].astype(str).tolist()
@@ -33,9 +27,6 @@ def extract_documents_sheet(df):
         x.replace("http://dx.doi.org/", "") if "doi" in x else None
         for x in source_links
     ]
-
-    # Extract first_author column
-    first_authors = [""] * num_documents
 
     # Extract year column
     years = documents["Year"].tolist()
@@ -53,11 +44,8 @@ def extract_documents_sheet(df):
     return {
         "id": ids,
         "document_type": document_types,
-        "pmid": pmids,
         "other_study_identifier": other_study_identifiers,
-        "extracted": extracteds,
         "doi": dois,
-        "first_author": first_authors,
         "year": years,
         "title": titles,
         "url": urls,
@@ -506,58 +494,82 @@ def extract_conc_time_values_sheet(df):
     }
 
 
-def create_template(df, template_name):
-    # Create dataframes for the sheets
-    documents_df = pd.DataFrame(extract_documents_sheet(df))
-    studies_df = pd.DataFrame(extract_studies_sheet(df))
-    subjects_df = pd.DataFrame(extract_subjects_sheet(df))
-    series_df = pd.DataFrame(extract_series_sheet(df))
-    conc_time_values_df = pd.DataFrame(extract_conc_time_values_sheet(df))
+def create_template(df, template_name, in_template):
+    for sheet_name in in_template.keys():
+        # Call extraction function dynamically
+        s_df = pd.DataFrame(globals()[f'extract_{sheet_name.lower()}_sheet'](df))
+        # Find missing cols
+        s_missing = list(set(in_template[sheet_name].columns) - 
+                         set(s_df.columns))
+        s_df[s_missing] = np.nan
+        # Check if file exists, write new or append
+        if not os.path.isfile(template_name):
+            # print(f"Writing new XLSX for {sheet_name}")
+            with pd.ExcelWriter(template_name, engine="openpyxl") as writer:
+                # Re-order fields and append to XLSX
+                s_df[in_template[sheet_name].columns.tolist()].to_excel(writer, sheet_name=sheet_name, index=False)
+        else:
+            # print(f"Appending to XLSX for {sheet_name}")
+            with pd.ExcelWriter(template_name, engine="openpyxl", mode='a') as writer:
+                # Re-order fields and append to XLSX
+                s_df[in_template[sheet_name].columns.tolist()].to_excel(writer, sheet_name=sheet_name, index=False)
+        
+    # # Create dataframes for the sheets
+    # documents_df = pd.DataFrame(extract_documents_sheet(df)) 
+    # studies_df = pd.DataFrame(extract_studies_sheet(df, in_template['Studies']))
+    # subjects_df = pd.DataFrame(extract_subjects_sheet(df, in_template['Subjects']))
+    # series_df = pd.DataFrame(extract_series_sheet(df, in_template['Series']))
+    # conc_time_values_df = pd.DataFrame(extract_conc_time_values_sheet(df, in_template['Conc_Time_Values']))
 
-    # Create excel file
-    with pd.ExcelWriter(template_name, engine="xlsxwriter") as writer:
-        documents_df.to_excel(writer, sheet_name="Documents", index=False)
-        studies_df.to_excel(writer, sheet_name="Studies", index=False)
-        subjects_df.to_excel(writer, sheet_name="Subjects", index=False)
-        series_df.to_excel(writer, sheet_name="Series", index=False)
-        conc_time_values_df.to_excel(writer, sheet_name="Conc_Time_Values", index=False)
+    # # Create excel file
+    # with pd.ExcelWriter(template_name, engine="xlsxwriter", mode='a') as writer:
+    #     documents_df[in_template['Documents'].columns.tolist()].to_excel(writer, sheet_name="Documents", index=False)
+    #     studies_df.to_excel(writer, sheet_name="Studies", index=False)
+    #     subjects_df.to_excel(writer, sheet_name="Subjects", index=False)
+    #     series_df.to_excel(writer, sheet_name="Series", index=False)
+    #     conc_time_values_df.to_excel(writer, sheet_name="Conc_Time_Values", index=False)
 
+if __name__ == "__main__":
+    folder_path = os.getenv("in_dir")
+    dest_path = os.getenv("out_dir")
 
-folder_path = os.getenv("in_dir")
-dest_path = os.getenv("out_dir")
+    file_paths = os.listdir(folder_path)
+    
+    # Load CvT Template (help fill in columns)
+    cvt_template = pd.read_excel("input/CvT_data_template_articles.xlsx", None)
 
-file_paths = os.listdir(folder_path)
+    for file_path in file_paths:
+        try:
+            # get the main document
+            with warnings.catch_warnings(record=True):
+                warnings.simplefilter("always")
+                df = pd.read_excel(os.path.join(folder_path, file_path), engine="openpyxl")
 
-for file_path in file_paths:
-    try:
-        # get the main document
-        with warnings.catch_warnings(record=True):
-            warnings.simplefilter("always")
-            df = pd.read_excel(os.path.join(folder_path, file_path), engine="openpyxl")
+            # Filter out all documents that contain concomitants
+            df = df[
+                df["Concomitants"].astype(str).str.lower().isin(["fed", "fasted", "nan"])
+            ]
+            df["Document"] = (
+                df["Source Link"].astype(str).apply(lambda x: x.split("?", 1)[0])
+            )
+            df["Source Split"] = df["Source"].astype(str).apply(lambda x: x.split(",")[0])
 
-        # Filter out all documents that contain concomitants
-        df = df[
-            df["Concomitants"].astype(str).str.lower().isin(["fed", "fasted", "nan"])
-        ]
-        df["Document"] = (
-            df["Source Link"].astype(str).apply(lambda x: x.split("?", 1)[0])
-        )
-        df["Source Split"] = df["Source"].astype(str).apply(lambda x: x.split(",")[0])
+            # create a list of dataframes, one for each group
+            document_groups = df.groupby("Document")
+            doc_dfs = []
 
-        # create a list of dataframes, one for each group
-        document_groups = df.groupby("Document")
-        doc_dfs = []
+            for name, group in document_groups:
+                doc_dfs.append(group)
 
-        for name, group in document_groups:
-            doc_dfs.append(group)
-
-        for i, doc_df in enumerate(doc_dfs):
-            filename = doc_df["filename"].head(1).values[0]
-            if filename is None:
-                filename = (
-                    "%030x" % random.randrange(16**30) + ".pdf"
-                )  # Random hexstring for filename
-            template_name = str(filename).replace(".pdf", ".xlsx")
-            create_template(doc_df, os.path.join(dest_path, template_name))
-    except:
-        print(f"error at filepath {file_path}")
+            for i, doc_df in enumerate(doc_dfs):
+                filename = doc_df["filename"].head(1).values[0]
+                if filename is None:
+                    filename = (
+                        "%030x" % random.randrange(16**30) + ".pdf"
+                    )  # Random hexstring for filename
+                template_name = str(filename).replace(".pdf", ".xlsx")
+                create_template(doc_df, 
+                                os.path.join(dest_path, template_name),
+                                cvt_template)
+        except:
+            print(f"error at filepath {file_path}")
