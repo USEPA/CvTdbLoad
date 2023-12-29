eeb06e4a23417947ee222de9f26f30ed181bf0b3
bkesic <138720026+bkesic@users.noreply.github.com>
2023-12-29 12:17:52 -0500

    Completed required field checking
    Added a special case for fk_reference_document_id in check_required_fields_validator, since it spans multiple sheets it could not be included in the YAML file. Also added the rule for figure_series_identifier using is_unique check for figure_name

--- R/check_required_fields_validator.R
+++ R/check_required_fields_validator.R
@@ -16,19 +16,28 @@
 #' @export 
 #' @importFrom
 check_required_fields_validator <- function(df, f){
-  # Loop through each sheet and individual rules
+  # Loop through each sheet
   for (sheet in names(df)) {
+    # Pull rules from YAML files
     rules <- validate::validator(.file=paste0("input/rules/", sheet,".yaml"))
+    
+    # Special case check for fk_reference_document_id
+    if (sheet == "Series" && nrow(df[["Documents"]]) > 1) {
+      rule <- validate::validator(!is.na(fk_reference_document_id))
+      validate::meta(rule, "message") <- "NA_in_required_field_fk_reference_document_id"
+      rules <- c(rules, rule)
+    }
+    
+    # Obtain a dataframe of failing validation checks
     out <- validate::confront(df[[sheet]], rules)
     fails = validate::summary(out) %>% 
       dplyr::filter(fails > 0)
     
     # Loop through each failure and log the message
-    if (nrow(fails)) {
-      for (i in seq_len(nrow(fails))) {
-        m = validate::meta(rules[fails$name[i]])$message
-        log_CvT_doc_load(f=f, m=m)
-      }
+    for (i in seq_len(nrow(fails))) {
+      m = validate::meta(rules[fails$name[i]])$message
+      message(m)
+      log_CvT_doc_load(f=f, m=m)
     }
   }
 }
