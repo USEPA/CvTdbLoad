959929781aa8c92f6224de05574bf8d9338c0f9f
bkesic <138720026+bkesic@users.noreply.github.com>
2024-03-29 11:52:22 -0400

    Add record categorization, psuedocode for further steps

--- scripts/r_load_CvT/qc_to_db.R
+++ scripts/r_load_CvT/qc_to_db.R
@@ -8,36 +8,49 @@
 # 4. A new record is added
 #       - Add this new record to the database
 
+# Conditionals are built to safely handle quality control issues
+#   - For example, someone might put "PASS" on a new record which should not have a qc_status.
+#     This could produce duplicate columns if conditionals are not carefully chosen
+
 qc_to_cvt <- function(f) {
     qc_template <- "input/qc_template.xlsx"
     field_mapping <- "input/qa_template_map.xlsx"
     doc_sheet_list <- load_sheet_group(fileName = f, template_path = qc_template)
 
     for (sheet in names(doc_sheet_list)) {
-        # Handle failed records
-        ids_to_remove <- doc_sheet_list[[sheet]] %>% 
-            dplyr::filter(qc_status == "FAIL" || qc_flags == "Not Suitable" || qc_flags == "Split entry, original")  %>%
-            dplyr::select(id)
+        categorized_records <- doc_sheet_list[[sheet]] %>%
+            dplyr::mutate(
+                category = case_when(
+                    qc_status == "FAIL" ~ "Remove",
+                    qc_flags == "Modified" ~ "Update",
+                    qc_flags == "New entry" | stringr::str_detect(qc_flags, "Split entry") ~ "Add",
+                    TRUE ~ "Ignore"
+                )
+            ) %>%
+            dplyr::select(id, category)
+
         # TODO: Remove these ids from the database matching sheet to db table
-        # "DELETE FROM cvt.{sheet} WHERE id = {id}"
+        ids_to_remove <- filter(categorized_records, category == "Remove")
+        # query <- "DELETE FROM cvt.{sheet} WHERE id = {id}"
 
-        # All remaining records are assumed to pass (QC SOP)
-        pass_records <- doc_sheet_list[[sheet]] %>% 
-            dplyr::filter(qc_status != "FAIL" && qc_flags != "Not Suitable" && qc_flags != "Split entry, original")
-
-        # Handle modified records
-        ids_to_update <- pass_records %>%
-            dplyr::filter(qc_flags == "Modified") %>% 
-            dplyr::select(id)
         # TODO: Map these to their database columns, while normalizing relevant fields
         # Update any fields that have changed
+        ids_to_update <- filter(categorized_records, category == "Update")
+        # query <- "UPDATE cvt.{sheet} SET record = new_df[record][ids_to_update]"
 
-        # Handle new records
-        ids_to_add <- pass_records %>% 
-            dplyr::filter(qc_flags == "New entry" || qc_flags == "Split entry") %>% 
-            dplyr::select(id)
         # TODO: Map these to their database columns, while normalizing relevant fields
         # Add these new rows to the database
+        ids_to_add <- filter(categorized_records, category == "Add")
+        # query <- "INSERT INTO cvt.{sheet} (columns) VALUES records[ids_to_add]"
+        
+        # Likely shouldn't need to touch these passing, unmodified records
+        ids_to_ignore <- filter(categorized_records, category == "Ignore")
+
+        # TODO: normalize_dose, normalize_time, normalize_conc, ideally only once
+        # updated_sheet = doc_sheet_list[[sheet]] %>%
+        #                     dplyr::mutate(
+        #                        field_mapping[column] = column
+        #                     )
     }
 }
 
