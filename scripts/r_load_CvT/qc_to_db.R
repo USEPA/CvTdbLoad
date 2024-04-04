990155a6c0da582f8570beb5e9b3f6e8835ce6e3
bkesic <138720026+bkesic@users.noreply.github.com>
2024-04-04 17:22:43 -0400

    Fixed parameter names, Completed mapping function, Improved performance/comments

--- scripts/r_load_CvT/qc_to_db.R
+++ scripts/r_load_CvT/qc_to_db.R
@@ -8,18 +8,17 @@
 # 4. A new record is added
 #       - Add this new record to the database
 
-# Conditionals are built to safely handle quality control issues
-#   - For example, someone might put "PASS" on a new record which should not have a qc_status.
-#     This could produce duplicate columns if conditionals are not carefully chosen
+# TODO: Maybe validate cvt fields alongside validating qc fields. So that we can rename all the columns, if we need all the columns. Currently just skipping non-existing columns, which might be a bad idea.
+# TODO: Still need to handle normalization in this workflow
 
 validate_qc_fields <- function(df) {
     validation <- TRUE # False if an invalid condition was encountered
     rules <- validate::validator(.file=paste0("input/rules/QC.yaml"))
     
     # Loop through each sheet
-    for (sheet in names(df)) {
+    for (sheet_name in names(df)) {
         # Pull rules from the respective YAML, and store failing validation checks
-        out <- validate::confront(df[[sheet]], rules)
+        out <- validate::confront(df[[sheet_name]], rules)
         fails <- validate::summary(out) %>% 
             dplyr::filter(fails > 0)
 
@@ -27,12 +26,15 @@ validate_qc_fields <- function(df) {
         for (i in seq_len(nrow(fails))) {
             validation = FALSE
             m <- validate::meta(rules[fails$name[i]])$message
-            message(m)
+            message(paste0(sheet_name, ": ", m))
             # log_CvT_doc_load(f=f, m=m, log_path=log_path)
         }
     }
 
-    return(validation)
+    if (isFALSE(validation)) {
+        message("Validation failed, exiting.")
+        stop()
+    }
 }
 
 qc_to_cvt <- function(f) {
@@ -40,36 +42,38 @@ qc_to_cvt <- function(f) {
     field_mapping <- readxl::read_xlsx("input/qa_template_map.xlsx")
 
     # Convert all qc_status and qc_flags to lowercase, for simpler comparison
-    for (sheet in names(doc_sheet_list)) {
-        doc_sheet_list[[sheet]] <- doc_sheet_list[[sheet]] %>%
+    for (sheet_name in names(doc_sheet_list)) {
+        doc_sheet_list[[sheet_name]] <- doc_sheet_list[[sheet_name]] %>%
             dplyr::mutate(
                 qc_status = tolower(qc_status),
                 qc_flags = tolower(qc_flags)
             )
     }
 
-    if (isFALSE(validate_qc_fields(doc_sheet_list))) {
-        message("Validation failed, exiting.")
-        quit()
-    }
+    # Validate that all qc_fields values are as expected
+    validate_qc_fields(doc_sheet_list)
 
-    for (sheet in names(doc_sheet_list)) {
+    for (sheet_name in names(doc_sheet_list)) {
         # Get the sheet mapping to remap the fields
         sheet_mapping <- field_mapping %>%
             dplyr::filter(sheet == tolower(sheet_name))
-    
+
         # Iterate over each row in the sheet_mapping and update the column name
         for (i in 1:nrow(sheet_mapping)) {
-            to_column <- sheet_mapping$to[i]
-            from_column <- sheet_mapping$from[i]
+            new_value <- sheet_mapping$to[i]
+            old_value <- sheet_mapping$from[i]
             
-            test[[sheet_name]] <- test[[sheet_name]] %>%
-                rename(!!from_column := !!sym(to_column))
+            # Rename the column if it exists in the sheet, otherwise skip it
+            if (old_value %in% doc_sheet_list[[sheet_name]]) {
+                doc_sheet_list[[sheet_name]] <- doc_sheet_list[[sheet_name]] %>%
+                    dplyr::rename(!!old_value := !!new_value)
+            }
         }
 
-        categorized_records <- doc_sheet_list[[sheet]] %>%
+        # Categorize each record based on 4 conditions of remove, update, add, or ignore
+        categorized_records <- doc_sheet_list[[sheet_name]] %>%
             dplyr::mutate(
-                category = case_when(
+                category = dplyr::case_when(
                     qc_status == "fail" ~ "Remove",
                     qc_flags == "modified" ~ "Update",
                     qc_flags == "new entry" | stringr::str_detect(qc_flags, "split entry") ~ "Add",
@@ -78,36 +82,29 @@ qc_to_cvt <- function(f) {
             ) %>%
             dplyr::select(id, category)
 
-        # TODO: Remove these ids from the database matching sheet to db table
+        # Remove these ids from the database
         ids_to_remove <- dplyr::filter(categorized_records, category == "Remove")
         # query <- "DELETE FROM cvt.{sheet} WHERE id = {id}"
 
-        # TODO: Map these to their database columns, while normalizing relevant fields
         # Update any fields that have changed
         ids_to_update <- dplyr::filter(categorized_records, category == "Update")
         # query <- "UPDATE cvt.{sheet} SET record = new_df[record][ids_to_update]"
 
-        # TODO: Map these to their database columns, while normalizing relevant fields
         # Add these new rows to the database
         ids_to_add <- dplyr::filter(categorized_records, category == "Add")
         # query <- "INSERT INTO cvt.{sheet} (columns) VALUES records[ids_to_add]"
         
         # Likely shouldn't need to touch these passing, unmodified records
         ids_to_ignore <- dplyr::filter(categorized_records, category == "Ignore")
-
-        # TODO: normalize_dose, normalize_time, normalize_conc, ideally only once
-        # updated_sheet = doc_sheet_list[[sheet]] %>%
-        #                     dplyr::mutate(
-        #                        field_mapping[column] = column
-        #                     )
     }
 }
 
 fileList <- c(
     "input/test_qc/PMID17668360_QC_template_bkesic_erowan_20240212.xlsx",
     "input/test_qc/test_validation.xlsx",
-    "input/test_qc/test_invalidation.xlsx"
+    "input/test_qc/test_invalidation.xlsx",
+    "input/test_qc/PMID24495244_QC_template_bkesic_mhuse_20240206.xlsx"
 )
-f <- fileList[[2]]
+f <- fileList[[1]]
 
 qc_to_cvt(f)
