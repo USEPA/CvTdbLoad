49362ce0cf1ef72b4ffba5398dbfab452fe54150
Wall <Wall.Taylor@epa.gov>
2025-05-02 13:14:16 -0400

    Updated release notes and vignette

--- scripts/release/summarize_cvt_data.R
+++ scripts/release/summarize_cvt_data.R
@@ -7,6 +7,11 @@ summarize_cvt_data <- function(){
     `Total Extraction Documents` = db_query_cvt(paste0("SELECT count(*) as n FROM cvt.documents ",
                                                        "WHERE id in (SELECT fk_extraction_document_id FROM cvt.studies)")) %>%
       dplyr::pull(n),
+    `Total Unique DTXSID Entries` = db_query_cvt(paste0("SELECT count(distinct dsstox_substance_id) as n FROM cvt.chemicals ",
+                                                        "WHERE dsstox_substance_id IS NOT NULL AND ",
+                                                        "(id IN (SELECT fk_dosed_chemical_id FROM cvt.studies) OR ",
+                                                        "id IN (SELECT fk_analyzed_chemical_id FROM cvt.series))")) %>%
+      dplyr::pull(n),
     `Total Studies` = db_query_cvt(paste0("SELECT count(*) as n FROM cvt.studies ",
                                           "WHERE id in (SELECT fk_study_id FROM cvt.series)")) %>%
       dplyr::pull(n),
@@ -23,7 +28,8 @@ summarize_cvt_data <- function(){
     `Species Breakdown` = db_query_cvt(paste0("SELECT species, count(*) as n FROM cvt.subjects ",
                                               "WHERE id in (SELECT fk_subject_id FROM cvt.series) ",
                                               "GROUP BY species")) %>%
-      tidyr::unite(col = "species_n", species, n, sep = "_") %>%
+      tidyr::unite(col = "species_n", species, n, sep = " (") %>%
+      dplyr::mutate(species_n = paste0(species_n, ")")) %>%
       dplyr::pull(species_n) %>%
       paste0(collapse = ", "),
     `Total Series` = db_query_cvt(paste0("SELECT count(*) as n FROM cvt.series WHERE id IN ",
@@ -37,11 +43,6 @@ summarize_cvt_data <- function(){
     `Total Analyzed Chemicals Mapped to DTXSID` = db_query_cvt(paste0("SELECT count(distinct fk_analyzed_chemical_id) as n FROM cvt.series ",
                                                                       "WHERE id in (SELECT fk_series_id FROM cvt.conc_time_values) AND ",
                                                                       "fk_analyzed_chemical_id in (SELECT id FROM cvt.chemicals WHERE dsstox_substance_id IS NOT NULL)")) %>%
-      dplyr::pull(n),
-    `Total Unique DTXSID Entries` = db_query_cvt(paste0("SELECT count(distinct dsstox_substance_id) as n FROM cvt.chemicals ",
-                                                        "WHERE dsstox_substance_id IS NOT NULL AND ",
-                                                        "(id IN (SELECT fk_dosed_chemical_id FROM cvt.studies) OR ",
-                                                        "id IN (SELECT fk_analyzed_chemical_id FROM cvt.series))")) %>%
       dplyr::pull(n),
     `Total Concentration-Time Values` = db_query_cvt(paste0("SELECT count(*) as n FROM cvt.conc_time_values ",
                                                             "WHERE fk_series_id in (SELECT id FROM cvt.series WHERE ",
