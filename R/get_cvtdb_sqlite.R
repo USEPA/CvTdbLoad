49d5492c4d3af7dd5e8a0d56907f6fb7a0944082
Taylor Wall <wall.taylor@epa.com>
2025-09-18 11:18:22 -0400

    Add exclude_audit param and logic

--- R/get_cvtdb_sqlite.R
+++ R/get_cvtdb_sqlite.R
@@ -2,6 +2,7 @@
 #' @description Pull CvTdb tables and write sqlite file.
 #' @param schema Database postgreSQL schema.
 #' @param outdir String for desired output directory.
+#' @param exclude_audit Boolean whether to exclude the "cvt_audit" table from the SQLite file due to size. Default is False.
 #' @return None. SQLite file is written to `outdir` folder.
 #' @seealso 
 #'  \code{\link[dplyr]{pull}}
@@ -12,9 +13,14 @@
 #' @importFrom dplyr pull
 #' @importFrom DBI dbConnect dbWriteTable dbDisconnect
 #' @importFrom RSQLite SQLite
-get_cvtdb_sqlite <- function(schema, outdir){
+get_cvtdb_sqlite <- function(schema, outdir, exclude_audit = FALSE){
   message("Getting CvT sqlite...")
   n_file = file.path(outdir, "res_cvtdb.sqlite")
+  
+  if(exclude_audit){
+    n_file = file.path(outdir, "res_cvtdb_no_audit.sqlite")
+    audit_file = file.path(outdir, "audit.parquet")
+  }
   
   if(file.exists(n_file)) file.remove(n_file)
   
@@ -25,10 +31,18 @@ get_cvtdb_sqlite <- function(schema, outdir){
   con = DBI::dbConnect(RSQLite::SQLite(), dbname = n_file)
   
   for(tbl in table_list){
-    message("...Writing table ", tbl)
+    tbl_df = db_query_cvt(paste0("SELECT * FROM ", schema, ".", tbl))
+    # If exclude_audit, store as arrow file
+    if(tbl == "cvt_audit" & exclude_audit){
+      message("...Writing table ", tbl, " to parquet file...")
+      arrow::write_parquet(tbl_df, audit_file)
+      # Skip adding to SQLite file
+      next
+    }
+    message("...Writing table ", tbl, " to sqlite file...")
     DBI::dbWriteTable(conn = con,
                      name = tbl,
-                     value = db_query_cvt(paste0("SELECT * FROM ", schema, ".", tbl)))
+                     value = tbl_df)
   }
   DBI::dbDisconnect(con)
   message("Done.")
